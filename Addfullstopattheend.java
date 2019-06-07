0244e47f0270659809246c9984e31d687432c989
saravana <saravanasaru14313@gmail.com>
2019-06-07 11:50:46 +0530

    Update and rename README.md to Addfullstopattheend.java

---
+++ Addfullstopattheend.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class Addfullstop
+{
+	public static void main(String[] args) 
+	{
+	    Scanner input=new Scanner(System.in);
+        String str1=input.nextLine();
+        String str=str1+'.';
+        System.out.println(str);
+	}
+}
